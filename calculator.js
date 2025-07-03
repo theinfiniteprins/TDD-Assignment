5bc73a0c1ca2a22a37bd3f218891b550a51ac211
PRINS <prinsbhuva2004@gmail.com>
2025-07-04 00:32:34 +0530

    Refactoring

--- calculator.js
+++ calculator.js
@@ -1,12 +1,12 @@
 function add(numbers) {
     if(numbers === "")return 0;
 
-    let nums = numbers.split(/,|\n/).map(Number);
+    let List_of_numbers = numbers.split(/,|\n/).map(Number);
     let sum = 0;
-    nums.forEach(element => {
+    List_of_numbers.forEach(element => {
         sum += element;
     });
     return sum;
- }
+ }  
 
 module.exports = { add };
