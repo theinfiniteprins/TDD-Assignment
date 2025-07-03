2ccdf5af8a1ade807cc78e432e81eb2117ffaf55
PRINS <prinsbhuva2004@gmail.com>
2025-07-04 00:04:26 +0530

    Failed : Step 1.2 Handle Single number input

--- calculator.spec.js
+++ calculator.spec.js
@@ -3,3 +3,8 @@ const { add } = require('./calculator'); // This file is empty now.
 test('should return 0 for empty string', () => {
     expect(add("")).toBe(0);
 });
+
+test('should return number itself for single number string', () => {
+    expect(add("1")).toBe(1);
+    expect(add("5")).toBe(5);
+});
