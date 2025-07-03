d6eeba5a097efda6a3761487164f65724fe7af02
PRINS <prinsbhuva2004@gmail.com>
2025-07-04 00:10:34 +0530

    Passed : Step 1.3 Handle two comma saperated number input

--- calculator.js
+++ calculator.js
@@ -1,7 +1,12 @@
 function add(numbers) {
     if(numbers === "")return 0;
 
-    return parseInt(numbers);
+    const nums = numbers.split(',').map(Number);
+    let sum = 0;
+    nums.forEach(element => {
+        sum += element;
+    });
+    return sum;
  }
 
 module.exports = { add };
