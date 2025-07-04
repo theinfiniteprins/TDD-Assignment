11b964545ebaee46ef40ad09869ee741997a3a7c
PRINS <prinsbhuva2004@gmail.com>
2025-07-04 20:45:34 +0530

    Passed : Step 4 handle custom delimiters

--- calculator.js
+++ calculator.js
@@ -2,6 +2,14 @@ function add(numbers) {
     if(numbers === "")return 0;
 
     let List_of_numbers = numbers.split(/,|\n/).map(Number);
+
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split('\n');
+        delimiter = new RegExp(parts[0].substring(2));
+        numbers = parts[1];
+        List_of_numbers = numbers.split(delimiter).map(Number);
+    }
+
     let sum = 0;
     List_of_numbers.forEach(element => {
         sum += element;
